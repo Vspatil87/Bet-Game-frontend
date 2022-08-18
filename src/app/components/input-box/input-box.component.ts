106cac016676c708ce1d41c1891f971a530d3067
Djs <shirsathpatil87@gmail.com>
2022-08-18 23:00:48 +0530

    taking input values and storing it in array in input-box component

--- src/app/components/input-box/input-box.component.ts
+++ src/app/components/input-box/input-box.component.ts
@@ -7,11 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputBoxComponent implements OnInit {
   totalInputBox: Array<number> = [];
+  inputValues: Array<number> = [];
   constructor() { 
     for (let i = 0; i < 100; i++) {
       this.totalInputBox[i] = i;         
     }
+    this.inputValues.length = 10000;
+    this.inputValues.fill(0);
+  }  
+  public getValue(input: string, i: number): void { 
+    let value: number = parseInt(input);
+    if (isNaN(value)) {
+      value = 0;
+    }
+    this.inputValues[i] = value;
   }
-
   ngOnInit(): void {}
 }
