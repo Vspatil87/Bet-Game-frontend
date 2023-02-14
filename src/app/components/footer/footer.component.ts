1ed14f043f0dd6b59e3aa7bc715b2395898180a7
Djs <shirsathpatil87@gmail.com>
2023-02-14 23:38:38 +0530

    Fixed the final UI design (900 * 680)

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  winningList: Array<number> = [];
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 0; i < 10; i++) {
+      this.winningList[i] = i;
+    }
   }
 
 }
